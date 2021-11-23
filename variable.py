24d3ed7502e05f97d6bc5a03e40ce09459f61b31
Keigo-Iwakuma <sym90chn@gmail.com>
2021-11-23 21:55:05 +0900

    finish step13

--- variable.py
+++ variable.py
@@ -29,16 +29,18 @@ class Variable:
 
 
 class Function:
-    def __call__(self, inputs):
+    def __call__(self, *inputs):
         xs = [x.data for x in inputs]
-        ys = self.forward(xs)
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
         outputs = [Variable(as_array(y)) for y in ys]
 
         for output in outputs:
             output.set_creator(self)
         self.inputs = inputs
         self.outputs = outputs
-        return outputs
+        return outputs if len(outputs) > 1 else outputs[0]
     
     def forward(self, xs):
         raise NotImplementedError()
@@ -48,10 +50,9 @@ class Function:
 
 
 class Add(Function):
-    def forward(self, xs):
-        x0, x1 = xs
+    def forward(self, x0, x1):
         y = x0 + x1
-        return (y,)
+        return y
 
 
 # class Square(Function):
@@ -99,8 +100,8 @@ def as_array(x):
 
 
 if __name__ == "__main__":
-    xs = [Variable(np.array(2)), Variable(np.array(3))]
+    x0 = Variable(np.array(2))
+    x1 = Variable(np.array(3))
     f = Add()
-    ys = f(xs)
-    y = ys[0]
+    y = f(x0, x1)
     print(y.data)
