47f3e85c9ca2c714dc3187cf91bc0aa48760fa3a
Keigo-Iwakuma <sym90chn@gmail.com>
2021-11-27 18:39:56 +0900

    finish step35

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from dethree import Variable
 import dethree.functions as F
-# from dethree.utils import plot_dot_graph
+from dethree.utils import plot_dot_graph
 
 
 if __name__ == "__main__":
-    x = Variable(np.linspace(-7, 7, 200))
-    y = F.sin(x)
+    x = Variable(np.array(1.0))
+    y = F.tanh(x)
+    x.name = "x"
+    y.name = "y"
     y.backward(create_graph=True)
 
-    logs = [y.data.flatten()]
-
-    for i in range(3):
-        logs.append(x.grad.data.flatten())
+    iters = 2
+    for i in range(iters):
         gx = x.grad
         x.cleargrad()
         gx.backward(create_graph=True)
     
-    labels = ["y=sin(x)", "y'", "y''", "y'''"]
-    for i, v in enumerate(logs):
-        plt.plot(x.data, logs[i], label=labels[i])
-    plt.legend(loc="lower right")
-    plt.show()
+    gx = x.grad
+    gx.name = "gx" + str(iters+1)
+    plot_dot_graph(gx, verbose=False, to_file="./figures/tanh.png")
