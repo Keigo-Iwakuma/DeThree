55991a02eb12d62a05f17a61f4ca0c794ceb89c3
Keigo-Iwakuma <sym90chn@gmail.com>
2021-11-27 18:11:24 +0900

    finish step32

--- dethree/__init__.py
+++ dethree/__init__.py
@@ -1,7 +1,17 @@
-is_simple_core = True
+is_simple_core = False
 
 if is_simple_core:
     from dethree.core_simple import (
+        Variable,
+        Function,
+        using_config,
+        no_grad,
+        as_array,
+        as_variable,
+        setup_variable,
+    )
+else:
+    from dethree.core import (
         Variable,
         Function,
         using_config,
