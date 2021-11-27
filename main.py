64eb3a88653a3ba1a76f72df17079d6d9cbc373a
Keigo-Iwakuma <sym90chn@gmail.com>
2021-11-27 17:28:02 +0900

    finish step29

--- main.py
+++ main.py
@@ -4,25 +4,25 @@ from dethree import Variable, Function
 from dethree.utils import plot_dot_graph
 
 
-def rosenbrock(x0, x1):
-    y = 100 * (x1 - x0 ** 2) ** 2 + (x0 -1) ** 2
+def f(x):
+    y = x ** 4 - 2 * x ** 2
     return y
 
 
+def gx2(x):
+    return 12 * x ** 2 - 4
+
+
 if __name__ == "__main__":
-    x0 = Variable(np.array(0.0))
-    x1 = Variable(np.array(2.0))
-    lr = 1e-3
-    iters = 10000
+    x = Variable(np.array(2.0))
+    iters = 10
 
     for i in range(iters):
-        print(x0, x1)
+        print(i, x)
 
-        y = rosenbrock(x0, x1)
+        y = f(x)
 
-        x0.cleargrad()
-        x1.cleargrad()
+        x.cleargrad()
         y.backward()
 
-        x0.data -= lr * x0.grad
-        x1.data -= lr * x1.grad
+        x.data -= x.grad / gx2(x.data)
