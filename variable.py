c2d0f5453ad9ec00fffa23630e0a1c5ed7a3a114
Keigo-Iwakuma <sym90chn@gmail.com>
2021-11-27 13:55:40 +0900

    finish step19

--- variable.py
+++ variable.py
@@ -22,15 +22,41 @@ def no_grad():
 
 
 class Variable:
-    def __init__(self, data):
+    def __init__(self, data, name=None):
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError(f"{type(data)} is not supported.")
 
         self.data = data
+        self.name = name
         self.grad = None
         self.creator = None
         self.generation = 0
+    
+    @property
+    def shape(self):
+        return self.data.shape
+    
+    @property
+    def ndim(self):
+        return self.data.ndim
+    
+    @property
+    def size(self):
+        return self.data.size
+    
+    @property
+    def dtype(self):
+        return self.data.dtype
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __repr__(self):
+        if self.data is None:
+            return "variable(None)"
+        p = str(self.data).replace("\n", "\n" + " " * 9)
+        return "variable(" + p + ")"
 
     def set_creator(self, func):
         self.creator = func
@@ -155,6 +181,12 @@ def as_array(x):
 
 
 if __name__ == "__main__":
-    with no_grad():
-        x = Variable(np.array(2.0))
-        y = square(x)
+    x_ = np.array([1,2,3])
+    print(x_)
+    x = Variable(x_)
+    print(x)
+
+    x_ = np.array([[1,2,3], [4,5,6]])
+    print(x_)
+    x = Variable(x_)
+    print(x)
