44d19cef079df0cc8fb3b95c241958c4a343adff
Keigo-Iwakuma <sym90chn@gmail.com>
2021-12-02 22:24:32 +0900

    finish step40

--- dethree/core.py
+++ dethree/core.py
@@ -84,6 +84,9 @@ class Variable:
     def T(self):
         return dethree.functions.transpose(self)
     
+    def sum(self, axis=None, keepdims=False):
+        return dethree.functions.sum(self, axis, keepdims)
+    
     def set_creator(self, func):
         self.creator = func
         self.generation = func.generation + 1
@@ -172,11 +175,16 @@ class Function:
 # ================================================================================
 class Add(Function):
     def forward(self, x0, x1):
+        self.x0_shape, self.x1_shape = x0.shape, x1.shape
         y = x0 + x1
         return y
     
     def backward(self, gy):
-        return gy, gy
+        gx0, gx1 = gy, gy
+        if self.x0_shape != self.x1_shape:
+            gx0 = dethree.functions.sum_to(gx0, self.x0_shape)
+            gx1 = dethree.functions.sum_to(gx1, self.x1_shape)
+        return gx0, gx1
 
 
 def add(x0, x1):
--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from dethree.utils import plot_dot_graph
 
 
 if __name__ == "__main__":
-    x = Variable(np.array([[1,2,3,],[4,5,6,]]))
-    x = x.reshape(2, 3)
-    x = x.T
-    y = F.transpose(x)
-    y.backward(retain_grad=True)
-    print(x.grad)
+    x0 = Variable(np.array([[1,2,3,]]))
+    x1 = Variable(np.array([10]))
+    y = x0 + x1
+    print(y)
+
+    y.backward()
+    print(x1.grad)
